d0c19bdbd41b6783639ebaed3683d773a9721bbe
Uhani3824 <141938179+Uhani3824@users.noreply.github.com>
2023-09-08 10:55:20 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,62 @@
+// Question-1
+// Output Questions
+
+// Task-1
+console.log("Hello World!")
+
+// Task-2
+console.log("My name is Umm-e-Hani")
+console.log("I am Learning JavaScript via Full Stack Bootcamp of DiaaTech")
+console.log("I will become the best developer by hard work.")
+console.log("I am making commitment to do code with consistency.")
+
+// Variables
+// Task-1
+var name="Umm-e-Hani"
+var age=21
+
+console.log(name+age)
+
+// Task-2
+var name="Umm-e-Hani"
+var learning="I am Learning JavaScript via Full Stack Bootcamp of DiaaTech"
+var aim="I will become the best developer by hard work."
+var commitment="I am making commitment to do code with consistency."
+
+console.log("My name is "+name)
+console.log(learning)
+console.log(aim)
+console.log(commitment)
+
+// Task-3
+var num1=15
+var num2=10
+
+var sum=num1+num2
+var minus=num1-num2
+var multiply=num1*num2
+var divide=num1/num2
+
+console.log("Sum of numbers: "+sum)
+console.log("Difference of numbers: "+minus)
+console.log("Product of numbers: "+multiply)
+console.log("Division of numbers: "+divide)
+
+// BMI
+// Task-1
+var johnMass=35
+var markMass=50
+var johnHeight=5.0
+var markHeight=6.5
+
+// Task-2
+var johnBmi=johnMass / johnHeight ** 2
+var markBmi=markMass / markHeight ** 2
+
+console.log("John's BMI: "+johnBmi)
+console.log("Mark's BMI: "+markBmi)
+
+// Task-3
+var markHigherBmi= false
+
+console.log("Does Mark have a higher BMI than John: "+markHigherBmi)
